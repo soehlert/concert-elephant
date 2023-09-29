7c7ead9f5c5a4a69ad3942ebc6d89706630afef8
Sam Oehlert <sam.oehlert@gmail.com>
2023-09-29 00:54:25 -0500

    feat(dynamism): using pure ajax and javascript get the features we have been after: autocomplete artist/venue, modal popup that allows you to create a new artist or venue and carry on creation from then

--- concerts/urls.py
+++ concerts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from .views import ArtistAutocomplete, VenueAutocomplete
 
-app_name = "route_manager"
+app_name = "concerts"
 
 urlpatterns = [
     path("", views.home_page, name="home"),
