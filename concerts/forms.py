a7b16b9f1cda5229becec1c1f159a93045ddb5ab
Sam Oehlert <sam.oehlert@gmail.com>
2023-09-29 01:01:12 -0500

    refactor(clean-up): remove commented code

--- concerts/forms.py
+++ concerts/forms.py
@@ -6,22 +6,12 @@ from .models import Artist, Concert, Venue
 
 
 class ConcertForm(forms.ModelForm):
-    # artist = forms.ModelChoiceField(
-    #     queryset=Artist.objects.all(),
-    #     widget=forms.Select(attrs={"class": "form-control", "id": "artist-autocomplete"}),
-    # )
-
     class Meta:
         model = Concert
         fields = ["artist", "venue", "date", "opener", "festival"]
         widgets = {
             "date": DatePickerInput(options={"format": "MM/DD/YYYY"}, attrs={"class": "form-control"}),
         }
-
-    #
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields["artist"].queryset = Artist.objects.all()
 
 
 class ArtistForm(forms.ModelForm):
