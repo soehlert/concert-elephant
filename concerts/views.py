6202059936910afcdb4878b70dbc99cbec0657f2
Sam Oehlert <sam.oehlert@gmail.com>
2023-09-29 01:04:54 -0500

    feat(ajax): move autocomplete to be friendly to ajax as well as overriding some class methods to aid in modal creation of artist/venue, and generally make sure we can still accept a concert create form

--- concerts/views.py
+++ concerts/views.py
@@ -1,7 +1,9 @@
 from dal import autocomplete
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count, Q
-from django.shortcuts import redirect, render, reverse
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.generic import CreateView, DetailView, ListView
 
 from .forms import ArtistForm, ConcertForm, VenueForm
@@ -40,27 +42,30 @@ def main_search(request):
 
 class ArtistAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        qs = Artist.objects.all()
+        term = self.request.GET.get("term", "")
+        return Artist.objects.filter(name__icontains=term)
 
-        if self.q:
-            qs = qs.filter(name__istartswith=self.q)
-
-        return qs
+    def render_to_response(self, context):
+        artists = self.get_queryset()
+        artist_data = [{"id": artist.id, "label": artist.name} for artist in artists]
+        return JsonResponse(artist_data, safe=False)
 
 
 class VenueAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        qs = Venue.objects.all()
+        term = self.request.GET.get("term", "")
+        return Venue.objects.filter(name__icontains=term)
 
-        if self.q:
-            qs = qs.filter(name__istartswith=self.q)
-
-        return qs
+    def render_to_response(self, context):
+        venues = self.get_queryset()
+        # Return both the name (label) and the ID (value) for each venue
+        data = [{"id": venue.id, "label": venue.name} for venue in venues]
+        return JsonResponse(data, safe=False)
 
 
 @login_required
 def attend_concert(request, pk):
-    concert = Concert.objects.get(pk=pk)
+    concert = get_object_or_404(Concert, pk=pk)
     Concert.attend_concert(request.user, concert)
 
     return redirect("concerts:concert-list")
@@ -70,6 +75,9 @@ class ArtistListView(ListView):
     model = Artist
     template_name = "concerts/artist_list.html"
     paginate_by = 20
+
+    def get_queryset(self):
+        return Artist.objects.all().order_by("-id")
 
 
 class ArtistDetailView(DetailView):
@@ -88,6 +96,33 @@ class ArtistCreateView(CreateView):
 
     def get_success_url(self):
         return reverse("concerts:artist-list")
+
+    def form_valid(self, form):
+        artist_instance = form.save()
+
+        # Check if the request is AJAX
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            data = {
+                "status": "success",
+                "artist": artist_instance.name,
+                "artistId": artist_instance.id,
+                "message": "Artist created successfully",
+            }
+            return JsonResponse(data)
+
+        # For non-AJAX requests:
+        messages.success(self.request, "Artist created successfully.")
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        # Check if the request is AJAX
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            errors = {field: error[0] for field, error in form.errors.items()}
+            return JsonResponse({"status": "error", "errors": errors}, status=400)
+
+        # For non-AJAX requests:
+        messages.error(self.request, "There was an error creating the artist.")
+        return super().form_invalid(form)
 
 
 class VenueListView(ListView):
@@ -113,6 +148,36 @@ class VenueCreateView(CreateView):
     def get_success_url(self):
         return reverse("concerts:venue-list")
 
+    def form_valid(self, form):
+        # Save the venue
+        venue = form.save()
+
+        # Check if the request is AJAX
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse(
+                {
+                    "status": "success",
+                    "message": "Venue successfully created!",
+                    "venue": {"id": venue.id, "name": venue.name, "city": venue.city, "country": venue.country.code},
+                }
+            )
+
+        # For non-AJAX requests:
+        messages.success(self.request, "Venue successfully created.")
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        # Prepare the error messages
+        errors = {field: error[0] for field, error in form.errors.items()}
+
+        # Check if the request is AJAX
+        if self.request.headers.get("x-requested-with") == "XMLHttpRequest":
+            return JsonResponse({"status": "error", "errors": errors}, status=400)
+
+        # For non-AJAX requests:
+        messages.error(self.request, "There was an error creating the venue.")
+        return self.render_to_response(self.get_context_data(form=form))
+
 
 class ConcertListView(ListView):
     model = Concert
@@ -136,3 +201,33 @@ class ConcertCreateView(CreateView):
 
     def get_success_url(self):
         return reverse("concerts:concert-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["artist_form"] = ArtistForm()
+        context["venue_form"] = VenueForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        print("POST request received for ConcertCreateView")
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Get the artist name from the form data
+        artist_name = form.cleaned_data.get("artist")
+
+        # Find or create the artist instance by name
+        artist, created = Artist.objects.get_or_create(name=artist_name)
+
+        # Set the artist instance on the form's instance
+        form.instance.artist = artist
+
+        messages.success(self.request, "Concert successfully created!")
+
+        # Continue with the regular form save
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print("Form errors:", form.errors)
+        messages.error(self.request, "There was an error creating the concert.")
+        return super().form_invalid(form)
