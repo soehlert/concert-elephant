4101bcd2fda928612f08afd5b30d8ef3c63ab024
Sam Oehlert <sam.oehlert@gmail.com>
2023-09-29 20:08:08 -0500

    feat(openers): update our form to handle openers

--- concerts/forms.py
+++ concerts/forms.py
@@ -13,6 +13,43 @@ class ConcertForm(forms.ModelForm):
             "date": DatePickerInput(options={"format": "MM/DD/YYYY"}, attrs={"class": "form-control"}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        opener_fields = {key: forms.CharField(required=False) for key in self.data if key.startswith("opener-")}
+        self.fields.update(opener_fields)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        opener_artists = []
+
+        # Create a list of keys to check before modifying cleaned_data
+        opener_keys = [key for key in cleaned_data if key.startswith("opener-")]
+
+        for key in opener_keys:
+            value = cleaned_data.get(key)
+            print(f"{key} = {value}")
+            if value:
+                try:
+                    artist = Artist.objects.get(id=value)
+                    opener_artists.append(artist)
+                except Artist.DoesNotExist:
+                    self.add_error(key, f"Artist named {value} does not exist.")
+
+        cleaned_data["opener"] = opener_artists
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if commit:
+            instance.save()  # Save the Concert instance first to get an ID.
+
+            # Associate the artists with the opener.
+            for artist in self.cleaned_data["opener"]:
+                instance.opener.add(artist)
+
+        return instance
+
 
 class ArtistForm(forms.ModelForm):
     class Meta:
