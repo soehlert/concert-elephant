8cfdd29bd64832d08964db4366728d71f7e7c98e
Sam Oehlert <sam.oehlert@gmail.com>
2023-10-10 03:13:45 -0500

    build(tests): fix scaffolding for testing
    DRF Client is better at headers, track last_id for later use, flush all to make sure we are getting a fresh DB on each run

--- concerts/tests/features/environment.py
+++ concerts/tests/features/environment.py
@@ -1,19 +1,23 @@
 import logging
 
 from django.contrib.auth import get_user_model
-from django.test import Client
+from django.core.management import call_command
+from rest_framework.test import APIClient
 
 logging.basicConfig(level=logging.INFO)
 
 
 def before_all(context):
-    context.client = Client()
+    context.client = APIClient()
     User = get_user_model()
     if not User.objects.filter(username="testuser").exists():
         context.user = User.objects.create_user(username="testuser", password="testpassword")
     else:
         context.user = User.objects.get(username="testuser")
 
+    context.last_artist_id = None
+    context.last_venue_id = None
+
 
 def after_all(context):
-    context.user.delete()
+    call_command("flush", interactive=False, verbosity=0)  # This resets the database state
