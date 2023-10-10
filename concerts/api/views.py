81ad15926bfc88d3bd0d6c1373221453b4eccb90
Sam Oehlert <sam.oehlert@gmail.com>
2023-10-10 03:05:46 -0500

    feat(API): greatly update the functionality of our API
    hyperlinked fields, the concert reviews, getting facts about the models, ensuring permissions on different viewsets as well as views inside them, etc

--- concerts/api/views.py
+++ concerts/api/views.py
@@ -1,19 +1,73 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
 
-from ..models import Artist, Concert, Venue
-from .serializers import ArtistSerializer, ConcertSerializer, VenueSerializer
+from ..models import Artist, Concert, ConcertReview, Venue
+from .serializers import ArtistSerializer, ConcertReviewSerializer, ConcertSerializer, VenueSerializer
 
 
-class ArtistViewset(viewsets.ModelViewSet):
+class ArtistViewset(viewsets.GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
 
+    def get_permissions(self):
+        print(self.request.headers)
+        if self.action == "create":
+            return [permissions.IsAuthenticated()]
+        elif self.action == "list":
+            return [permissions.AllowAny()]
+        return super().get_permissions()
 
-class VenueViewset(viewsets.ModelViewSet):
+
+class VenueViewset(viewsets.GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
 
+    def get_permissions(self):
+        print(self.request.headers)
+        if self.action == "create":
+            return [permissions.IsAuthenticated()]
+        elif self.action == "list":
+            return [permissions.AllowAny()]
+        return super().get_permissions()
 
-class ConcertViewset(viewsets.ModelViewSet):
+
+class ConcertViewset(viewsets.GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin):
     queryset = Concert.objects.all()
     serializer_class = ConcertSerializer
+
+    def get_permissions(self):
+        print(self.request.headers)
+        if self.action == "create":
+            return [permissions.IsAuthenticated()]
+        elif self.action == "list":
+            return [permissions.AllowAny()]
+        return super().get_permissions()
+
+
+class ConcertReviewViewset(viewsets.ModelViewSet):
+    queryset = ConcertReview.objects.all()
+    serializer_class = ConcertReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Only let the author see their own reviews, nobody else
+    # This does mean you'll get a 404 instead of a 403 when trying to
+    # update/delete someone else's review by ID - privacy
+    def get_queryset(self):
+        return ConcertReview.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        review = self.get_object()
+        if self.request.user != review.user:
+            raise PermissionDenied("You can't edit this review!")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        review = self.get_object()
+        if self.request.user != review.user:
+            raise PermissionDenied("You can't delete this review!")
+        instance.delete()
