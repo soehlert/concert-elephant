ff25e0f7189abe854f826c4e3a5e39ce02b5c7f8
Sam Oehlert <sam.oehlert@gmail.com>
2023-10-15 01:22:36 -0500

    feat(sorting): add sorting to user detail (my profile page)
    we have to do it slightly differently here since its a detail view and not a list view. this is why were using the get_sorted_concerts method in our concert_list that is eventually passed to the template in the get_context_data method.

--- concert_elephant/users/views.py
+++ concert_elephant/users/views.py
@@ -1,8 +1,11 @@
+import logging
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
@@ -11,6 +14,7 @@ from concerts.forms import ConcertReviewForm
 from concerts.models import ConcertReview
 
 User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
@@ -18,6 +22,29 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
     paginate_by = 10
+
+    def get_sorted_concerts(self, user):
+        logger.warning(f"Unrecognized sort option received: {self.request.GET.get('sort_by')}")
+        sort_by = self.request.GET.get("sort_by", "-date")
+        sort_options = ["artist", "-artist", "venue", "-venue", "date", "-date"]
+
+        if sort_by not in sort_options:
+            sort_by = "-date"
+
+        secondary_sort = "date"
+
+        if sort_by in ["artist", "-artist"]:
+            concerts = user.concerts.annotate(lower_artist=Lower("artist__name")).order_by(
+                sort_by.replace("artist", "lower_artist"), secondary_sort
+            )
+        elif sort_by in ["venue", "-venue"]:
+            concerts = user.concerts.annotate(lower_venue=Lower("venue__name")).order_by(
+                sort_by.replace("venue", "lower_venue"), secondary_sort
+            )
+        else:
+            concerts = user.concerts.order_by(sort_by)
+
+        return concerts
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,7 +62,7 @@ class UserDetailView(LoginRequiredMixin, DetailView):
                 )
             ]
         else:
-            concert_list = [concert for concert in user.concerts.all()]
+            concert_list = [concert for concert in self.get_sorted_concerts(user)]
 
         for concert in concert_list:
             concert.user_review = ConcertReview.objects.filter(user=user, concert=concert).first()
